da738f918383f292d16139838a7bbe11d17d71ef
George Gerakinis <gerakinhs@hotmail.com>
2023-01-15 23:15:44 +0200

    hash md5

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const bodyParser= require('body-parser');
 
 const ejs = require("ejs");
 const mongoose =require("mongoose");
-const encrypt =require("mongoose-encryption");
+const  md5 =require("md5");
 
 const port=3000;
 
@@ -26,10 +26,6 @@ const userSchema = new mongoose.Schema( {
     email: String,
     password: String
 });
-
-////////////the encryption 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET ,encryptedFields:["password"]});
 
 
 const User =mongoose.model("User",userSchema);
@@ -49,7 +45,7 @@ app.get('/register',function(req,res){
 app.post("/register",function(req,res){
     newUser = new User({
         email:req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err){
@@ -62,7 +58,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password =req.body.password;
+    const password =md5(req.body.password);
 
     User.findOne({email:username},function(err,foundUser){
         if (err){
